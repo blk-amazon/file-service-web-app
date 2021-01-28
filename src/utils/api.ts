d899ab314b4c8fc1078f8fca1611c677ca9103bb
Brian Kilgore <blk@amazon.com>
2021-01-28 08:32:23 -0700

    REST API updates

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,9 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { apiBaseUrl } from '../config';
 
-const MasterControlAPI = (function() {
+export default (function() {
+  const partitionType = "db_nosql";
+
   return {
     getNewToken: async (user_id: string, tenant_id: string, headers?: object) => {
       const url = `${apiBaseUrl}/token`;
@@ -18,48 +20,22 @@ const MasterControlAPI = (function() {
       
       return axios.get(url, config);
     },
-    uploadFile: async (file: File, headers?: object) => {
-      console.log(file);
-      const fileName = file.name;
-
-      // const fileBuffer = await file.arrayBuffer();
-      // var uint8View = new Uint8Array(fileBuffer);
-      // console.log(uint8View);
-      // // const fileData = String.fromCharCode.apply(null, new Uint8Array(fileBuffer) as any);
-      // const fileData = new TextDecoder().decode(fileBuffer);
-      // // console.log(fileData);
-
-      // const url = `${apiBaseUrl}/object?partition=prefix`;
-
-      // const data = {
-      //   key: fileName,
-      //   value: uint8View,
-      // };
-      // console.log(data);
-
-      // const config = {
-      //   headers: {
-      //     ...headers,
-      //     'Content-Type': 'application/json',
-      //     'Access-Control-Allow-Origin': '*',
-      //   }
-      // };
-      // console.log(config);
-
-      // return axios.put(url, data, config);
+    putObject: async (object: File, headers?: object) => {
+      console.log(object);
+      const fileName = object.name;
 
       const fileReader = new FileReader();
-      fileReader.readAsDataURL(file);
+      fileReader.readAsDataURL(object);
 
       fileReader.onload = () => {
         // console.log("fileReader result", fileReader.result as string);
 
-        const url = `${apiBaseUrl}/object?partition=db_nosql`;
+        const url = `${apiBaseUrl}/object?partition=${partitionType}`;
 
         const data = {
           key: fileName,
           value: fileReader.result?.toString().replace(/data:\w+\/\w+;base64,/, ''),
-          content_type: file.type,
+          content_type: object.type,
         };
         // console.log(data);
   
@@ -75,8 +51,69 @@ const MasterControlAPI = (function() {
         return axios.put(url, data, config);
       }
     },
+    getObject: async (object: File, headers?: object) => {
+      console.log(object);
+      const fileName = object.name;
+
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(object);
+
+      fileReader.onload = () => {
+        // console.log("fileReader result", fileReader.result as string);
+
+        const url = `${apiBaseUrl}/object?partition=${partitionType}`;
+
+        const data = {
+          key: fileName,
+          value: fileReader.result?.toString().replace(/data:\w+\/\w+;base64,/, ''),
+          content_type: object.type,
+        };
+        // console.log(data);
+  
+        const config = {
+          headers: {
+            ...headers,
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+          }
+        };
+        console.log(config);
+  
+        return axios.put(url, data, config);
+      }
+    },
+    getObjectList: async (headers?: object) => {
+      const url = `${apiBaseUrl}/object?partition=${partitionType}`;
+      // console.log(data);
+
+      const config = {
+        headers: {
+          ...headers,
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        }
+      };
+      console.log(config);
+
+      return axios.get(url, config);
+    },
+    downloadFile: (bucket_name: string, key_name: string, headers?: object) => {
+      const url = `${apiBaseUrl}/download`;
+      const config: AxiosRequestConfig = {
+        headers: {
+          ...headers,
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+        params: {
+          'bucket_name': bucket_name,
+          'key_name': key_name,
+        }
+      };
+      console.log(config);
+      
+      return axios.get(url, config);
+    }
   }
 
 }());
-
-export default MasterControlAPI;
